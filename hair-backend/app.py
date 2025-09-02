6ab4ec2e1db32b5c1b52b169dac54b8107d44fc9
ubrowz <ubrowz@gmail.com>
2025-09-02 20:32:21 +0200

    Add files via upload
    changed endpoint from root / to /hair/

--- hair-backend/app.py
+++ hair-backend/app.py
@@ -63,7 +63,7 @@ def detect_hair_with_segmenter(image, hair_segmenter):
     return binary_hair_mask
 
 
-@app.post("/")
+@app.post("/hair/")
 async def segment_hair(file: UploadFile = File(...)):
     
     # Read uploaded image
