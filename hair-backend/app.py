3ea7369d0ab63b176194483b01dd68aaabdd6d23
ubrowz <ubrowz@gmail.com>
2025-09-04 17:59:02 +0200

    Add files via upload
    Another attempt to change the text position

--- hair-backend/app.py
+++ hair-backend/app.py
@@ -208,7 +208,7 @@ async def button_clicked(params: Parameters):
     currentAxis.add_patch(plt.Rectangle((0 ,0), 1, 1, facecolor=(0 ,0 ,0) ,transform=currentAxis.transAxes, zorder=-1))
     
     # Add text at specific coordinates
-    plt.text(-1, -1, f"Whipping Factor: {whipping_factor:.2f}" , fontsize=10, color="gray", ha="left")
+    plt.text(-5, -5, f"Whipping Factor: {whipping_factor:.2f}" , fontsize=10, color="gray", ha="left")
 
     
     buf = io.BytesIO()
